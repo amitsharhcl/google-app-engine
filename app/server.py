1d0c80ec34d6bb16ec50764823abed5afb199110
amitsharhcl <amit-sharma@hcl.com>
2019-07-13 19:07:48 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -7,7 +7,7 @@ from io import BytesIO
 
 from fastai.vision import *
 
-model_file_url = 'https://www.dropbox.com/s/y4kl2gv1akv7y4i/stage-2.pth?raw=1'
+model_file_url = 'https://drive.google.com/open?id=1tXT9KIWJywD0PsXArUl6q8U0o37OwryH'
 model_file_name = 'model'
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
